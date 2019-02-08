45c74da0d9660629abd28f293706b224b4b8101b
bgd0706 <bgd0706@hanmail.net>
2019-02-08 16:30:31 +0900

    Merge branch 'master' of https://github.com/bgd0706/bigdata2019
    # Conflicts:
    #	01. Jump to python/Chap00_pycharm_test/Korean_test.py

---
+++ 01. Jump to python/Chap04/d03.py
@@ -0,0 +1,23 @@
+def show_candidates(candidate_list) :
+    for i in candidate_list:
+        print(i)
+
+def make_idol(candidate_list) :
+    for i in candidate_list :
+        print("신예 아이돌 "+i+" 인기 급상승")
+
+def make_world_star(candidate_list) :
+    for i in candidate_list:
+        print("아이돌 " + i + " 월드스타 등극")
+
+f = open("연습생.txt", 'r', encoding="UTF-8")
+lines = f.readlines()
+f.close()
+
+candidate_list = []
+for i in lines :
+    candidate_list.append(i)
+
+show_candidates(candidate_list)
+make_idol(candidate_list)
+make_world_star(candidate_list)
