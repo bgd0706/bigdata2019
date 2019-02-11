b29ea8360fbad038a608b003a98b6daa5c1ecd74
bgd0706 <bgd0706@hanmail.net>
2019-02-11 09:32:32 +0900

    Merge branch 'master' of https://github.com/bgd0706/bigdata2019
    # Conflicts:
    #	01. Jump to python/Chap04/연습생.txt

--- 01. Jump to python/Chap04/d04.py
+++ 01. Jump to python/Chap04/d04.py
@@ -1,6 +1,6 @@
 def search_visitor(name) :
     f = open('방명록.txt', 'r', encoding='UTF-8')
-    lines = f.readlines()
+    lines = f.read().splitlines()
     for i in lines :
         info = i.split(" ")
         if (name == info[0]) :
@@ -10,6 +10,9 @@ def search_visitor(name) :
 while True :
     name = input("이름을 입력하세요: ")
     find = search_visitor(name)
+
+    if name == '종료' :
+        break
 
     if name == find:
         print('"'+find+"님 다시 방문해 주셔서 감사합니다. 즐거운 시간되세요.")
--- 01. Jump to python/Chap04/def01_greet_user.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-args = sys.argv[1:]
-for i in args:
-    print("Hello, "+i[0].upper()+i[1:])
-
-
