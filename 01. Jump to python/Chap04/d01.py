03a172f18280f7c345be3f0ee25cb7046e379672
bgd0706 <bgd0706@hanmail.net>
2019-02-11 09:35:11 +0900

    Merge branch 'master' of https://github.com/bgd0706/bigdata2019
    # Conflicts:
    #	01. Jump to python/Chap04/연습생.txt

--- 01. Jump to python/Chap04/d01.py
+++ 01. Jump to python/Chap04/d01.py
@@ -1,6 +1,12 @@
-def greet_users (usernames) :
-    for i in usernames :
-        print("Hello, " + i[0].upper() + i[1:]+'!')
+# def greet_users (usernames) :
+#     for i in usernames :
+#         print("Hello, " + i[0].upper() + i[1:]+'!')
 
-usernames = input("이름을 입력하세요 : ").split(" ")
-greet_users(usernames)
+# usernames = input("이름을 입력하세요 : ").split(" ")
+# greet_users(usernames)
+
+import sys
+
+args = sys.argv[3:]
+for i in args:
+    print("Hello, "+i[0].upper()+i[1:])
